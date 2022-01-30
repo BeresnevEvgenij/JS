edd559b8367723ad61efa94574e7c091c5feac11
neo <beresnevevgenij24@gmail.com>
2022-01-30 20:06:08 +0300

    ////

---
+++ JS/fail.js
@@ -0,0 +1,22 @@
+let out1 = document.querySelector('#out-1');
+document.querySelector('#btm-1').onclick = function() {
+  out1.value = "1";
+}
+
+console.log("Евгений");
+console.log(02);
+console.log('Добро '+'плжаловать '+'на курс!');
+
+let a = 7;
+let b = 9;
+console.log(a * b);
+
+let out2 = document.querySelector('#out-2');
+  document.querySelector('#btm-2').onclick = function () {
+    if (out2.value == 4) {
+      out2.value = true;
+    }
+    else {
+      out2.value = false;
+    }
+  }
--- fail.js
+++
@@ -1,4 +0,0 @@
-let out1 = document.querySelector('#out-1');
-document.querySelector('#btm-1').onclick = function() {
-  out1.value = "1";
-} 
